c91548ee59fe560f29a7bd64feaaed25080d4803
Carson Judge <cjudge1337@gmail.com>
2017-07-21 19:04:36 -0700

    Finish jade

--- program.js
+++ program.js
@@ -1,8 +1,20 @@
+const path = require('path');
 const express = require('express');
-const app = express();
+const app = express()
+
+app.set('views', path.join(process.argv[3]));
+app.set('view engine', 'jade')
+
+app.get('/home', (req, res) => {
+  res.render('index', {date: new Date().toDateString()})
+});
 
 app.listen(process.argv[2]);
-app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+// const express = require('express');
+// const app = express();
+//
+// app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+// app.listen(process.argv[2]);
 
 // const express = require('express');
 // const app = express();
