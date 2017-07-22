6f5e392a01a42ff69275dac1fd9a863a0e263515
Carson Judge <cjudge1337@gmail.com>
2017-07-21 18:47:00 -0700

    Finish static

--- program.js
+++ program.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/home', (req, res) => {
-  res.end("Hello World!");
-});
 app.listen(process.argv[2]);
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
+// const express = require('express');
+// const app = express();
+//
+// app.get('/home', (req, res) => {
+//   res.end("Hello World!");
+// });
+// app.listen(process.argv[2]);
