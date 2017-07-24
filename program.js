bd81d0f1642e560ec654e29709fc192f1290842b
Carson Judge <cjudge1337@gmail.com>
2017-07-24 15:43:48 -0700

    Finish stylish css

--- program.js
+++ program.js
@@ -1,15 +1,24 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
 
-app.post('/form', (req, res) => {
-  var string = req.body.str.split('').reverse().join('');
-  res.end(string);
-});
 
-app.listen(process.argv[2]);
+app.listen(process.argv[2])
+
+// const bodyParser = require('body-parser');
+// const express = require('express');
+// const app = express();
+//
+// app.use(bodyParser.urlencoded({extended: false}));
+//
+// app.post('/form', (req, res) => {
+//   var string = req.body.str.split('').reverse().join('');
+//   res.end(string);
+// });
+//
+// app.listen(process.argv[2]);
 
 // const path = require('path');
 // const express = require('express');
