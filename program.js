3b6f034ac0ac0ae19f63351ca3c36e49a47d6269
Carson Judge <cjudge1337@gmail.com>
2017-07-25 15:18:27 -0700

    Finish what's in query

--- program.js
+++ program.js
@@ -1,16 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.put('/message/:id', (req, res) => {
-  var str = require('crypto')
-        .createHash('sha1')
-        .update(new Date().toDateString() + req.params.id)
-        .digest('hex')
-
-  res.end(str);
+app.get('/search', (req, res) => {
+  res.send(req.query);
 });
 
 app.listen(process.argv[2]);
+
+// const express = require('express')
+// const app = express()
+//
+// app.put('/message/:id', (req, res) => {
+//   var str = require('crypto')
+//         .createHash('sha1')
+//         .update(new Date().toDateString() + req.params.id)
+//         .digest('hex')
+//
+//   res.end(str);
+// });
+//
+// app.listen(process.argv[2]);
 
 // const express = require('express')
 // const app = express()
